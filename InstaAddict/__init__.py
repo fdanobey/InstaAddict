7a6d1190c71932440c4b3fd6661815b761f531de
joeahkim <joeahkim@gmail.com>
2026-09-16 14:55:38 +0300

    Instagram version 447.0.0.55.81 BUILD

--- InstaAddict/__init__.py
+++ InstaAddict/__init__.py
@@ -1,7 +1,7 @@
 """InstaAddict - Human-like Instagram bot powered by UIAutomator2"""
 
 __version__ = "1.0.3"
-__tested_ig_version__ = "440.0.0.46.86"
+__tested_ig_version__ = "447.0.0.55.81"
 
 from InstaAddict.core.bot_flow import start_bot
 
